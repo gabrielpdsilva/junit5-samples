e468890b86c15cfe2dda948c77aa0bf8db5ba136
Gabrielpdsilva <Gabriel.pimentel.sotc@gmail.com>
2019-08-26 21:12:46 -0300

    novos testes realizados e metodos aperfeicoados, a maioria ainda necessita de reparacao

--- junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
@@ -18,45 +18,102 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class CalculatorTests {
-
+/*
 	@Test
 	@DisplayName("1 + 1 = 2")
 	void addsTwoNumbers() {
 		Calculator calculator = new Calculator();
-		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+		assertEquals(2, calculator.add(1, 1), "1 + 1 deveria ser 2");
 	}
 	
 	@Test
 	@DisplayName("2 - 1 = 1")
 	void subsTwoNumbers() {
 		Calculator calculator = new Calculator();
-		assertEquals(1, calculator.sub(2, 1), "2 - 1 should equal 1");
+		assertEquals(1, calculator.sub(2, 1), "2 - 1 deveria ser 1");
 	}
 	
 	@Test
 	@DisplayName("4 * 2 = 8")
 	void multsTwoNumbers() {
 		Calculator calculator = new Calculator();
-		assertEquals(8, calculator.mult(4, 2), "4 * 2 should equal 8");
+		assertEquals(8, calculator.mult(4, 2), "4 * 2 deveria ser 8");
 	}
 	
 	@Test
 	@DisplayName("8 / 2 = 4")
 	void divsTwoNumbers() {
 		Calculator calculator = new Calculator();
-		assertEquals(4, calculator.div(8, 2), "8 / 2 should equal 4");
-	}
+		assertEquals(4, calculator.div(8, 2), "8 / 2 deveria ser 4");
+	}*/
 
+	//======================================================================
+	
+	//método add
+	//teste de soma
 	@ParameterizedTest(name = "{0} + {1} = {2}")
 	@CsvSource({
 			"0,    1,   1",
-			"1,    2,   3",
+			"1.5,    2,   3.5",
 			"49,  51, 100",
-			"1,  100, 101"
+			"'a',  'a', 1",
+			"0,  0, 0",
+			"100, -101, -1"
 	})
-	void add(int first, int second, int expectedResult) {
+	void add(double first, double second, double expectedResult) {
 		Calculator calculator = new Calculator();
 		assertEquals(expectedResult, calculator.add(first, second),
-				() -> first + " + " + second + " should equal " + expectedResult);
+				() -> first + " + " + second + " deveria ser " + expectedResult);
+	}
+	
+	//método sub
+	//teste de subtração
+	@ParameterizedTest(name = "{0} - {1} = {2}")
+	@CsvSource({
+			"0,    -1,   -1",
+			"2.5,    1,   1.5",
+			"2,  2, 0",
+			"15,  10, 5",
+			"1,  1, 0",
+			"'a', 1, 1"
+	})
+	void sub(double first, double second, double expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.add(first, second),
+				() -> first + " - " + second + " deveria ser " + expectedResult);
+	}
+	
+	//método mult
+	//teste de multiplicação
+	@ParameterizedTest(name = "{0} * {1} = {2}")
+	@CsvSource({
+			"0,    1,   0",
+			"1.5,    2,   3",
+			"2,  2, 4",
+			"3,  -5, -15",
+			"2.5,  3, 7.5",
+			"'a', 1, 1"
+	})
+	void mult(double first, double second, double expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.add(first, second),
+				() -> first + " * " + second + " deveria ser " + expectedResult);
+	}
+	
+	//método div
+	//teste de divisão
+	@ParameterizedTest(name = "{0} / {1} = {2}")
+	@CsvSource({
+			"-8,    1,   -8",
+			"5,    2.5,   2",
+			"4,  4, 1",
+			"7,  2, 3.5",
+			"4,  2, 2",
+			"'a', 1, 1"
+	})
+	void div(double first, double second, double expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.add(first, second),
+				() -> first + " / " + second + " deveria ser " + expectedResult);
 	}
 }
